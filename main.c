321ce97739191253531586373d04d3be37a02334
Mikhail Semenkov <mih234bk@gmail.com>
2025-04-02 21:59:49 +0300

    Print text from file

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     printf("Hello World!\n");
@@ -7,5 +8,13 @@ int main() {
     printf("How are you??????????\n");
     printf("My commit is first!\n");
     printf("No, MYYYY commit is first!\n");
+    
+    FILE *fptr = NULL;
+    fptr = fopen("input.txt", "r");
+    char string[50] = "HELLO";
+    fgets(string, sizeof(string), fptr);
+    printf("%s\n", string);
+    fclose(fptr);
+
     return 0;
 }
