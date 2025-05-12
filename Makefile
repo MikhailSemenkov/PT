b0fb3d8a174fb55a57486de7dbc3a83581a311c5
Mikhail Semenkov <mih234bk@gmail.com>
2025-05-12 22:39:33 +0300

    Add unit tests

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+TEST_BUILD_DIR := build/tests
+
 build: main.c
 	mkdir -p build
 	gcc main.c operations.c -I include/ -o build/main
@@ -7,3 +9,12 @@ run: build
 
 clean:
 	rm -rf build/
+
+test: tests/
+	mkdir -p ${TEST_BUILD_DIR}
+	gcc -fprofile-arcs -ftest-coverage ./tests/test_operations.c operations.c ./Unity/src/unity.c -I ./include/ -I ./Unity/src/ \
+	-o ${TEST_BUILD_DIR}/test_operations
+	./${TEST_BUILD_DIR}/test_operations
+
+test_coverage: test
+	gcov --object-directory ${TEST_BUILD_DIR} --no-output test_operations-operations
---
+++ tests/test_operations.c
@@ -0,0 +1,39 @@
+#include <unity.h>
+#include "operations.h"
+
+void setUp() {
+}
+
+void tearDown() {
+}
+
+void test_add() {
+    TEST_ASSERT_EQUAL_INT(4, my_add(2, 2));
+}
+
+void test_sub() {
+    TEST_ASSERT_EQUAL_INT(0, my_sub(2, 2));
+}
+
+void test_mul() {
+    TEST_ASSERT_EQUAL_INT(4, my_mul(2, 2));
+}
+
+void test_div() {
+    TEST_ASSERT_EQUAL_INT(1, my_div(2, 2));
+}
+
+void test_pow() {
+    TEST_ASSERT_EQUAL_INT(4, my_pow(2, 2));
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_add);
+    RUN_TEST(test_sub);
+    RUN_TEST(test_mul);
+    RUN_TEST(test_div);
+    RUN_TEST(test_pow);
+    return UNITY_END();
+}
